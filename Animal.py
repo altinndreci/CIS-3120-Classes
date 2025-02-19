fc00eb5e0b0ce4d46384c3302c2e200c59ba8f24
Al <altinndreci@Altins-MacBook-Air.local>
2025-02-18 20:24:50 -0500

    Added new functions to the file

--- Animal.py
+++ Animal.py
@@ -1,7 +1,35 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, species, color, size, weight, owner): 
+        self.__name = name
+        self.__species = species
+        self.__color = color
+        self.__size = size
+        self.__weight = weight
+        self.__owner = owner
+        print("hello, i am", self.__name)
+        print( "I am a", self.__species)
+        print( "My color is", self.__color)
+        print( "My size is", self.__size)
+        print("My weight is", self.__weight)
+        print("My owner is", self.__owner)
+        
+    def noise(self, noise):
+        self.__noise = noise
+        print("My noise goes", self.__noise)
 
-    def talk(self):
-        print("hi")
+    def jump(self, jump):
+        self.__jump = jump
+        print("I can jump", self.__jump)
+
+    def playstyle(self, playstyle):
+        self.__playstyle = playstyle
+        print("I like to play", self.__playstyle)
+
+    def skin(self, skin):
+        self.__skin = skin
+        print("My skin is", self.__skin)
+
+    def age(self, age):
+        self.__age = age
+        print("I am ", self.__age, "years old")
 
